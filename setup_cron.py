3501da59da228694f1a4cce8ae746362c85f48a0
jigar wala <jigarwala007@gmail.com>
2019-09-08 17:49:23 +0530

    added cron tab support

---
+++ setup_cron.py
@@ -0,0 +1,35 @@
+from crontab import CronTab
+import os
+
+def setup_cron():
+    user_cron  = CronTab(user=True)
+    script_path = os.path.dirname(os.path.abspath(__file__))
+
+
+    for job in user_cron:
+        if script_path in job.command:
+            user_cron.remove(job)
+            user_cron.write()
+
+    command = f"python3 {os.path.join(script_path, 'script.py')}"
+
+    job = user_cron.new(command=command)
+
+    job.env["BASE_DIR"] = script_path
+
+    ## every morning 8 am uncomment when needed
+    job.hour.on(8)
+    ## for testing comment when done
+    # job.minute.every(1)
+
+    user_cron.write()
+
+    print('crontab set')
+
+    return True
+
+
+
+
+if __name__ == "__main__":
+    setup_cron()
