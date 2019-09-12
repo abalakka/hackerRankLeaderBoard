e80829aa79981cb0e9df87571ffeda959121df05
jigar wala <jigarwala007@gmail.com>
2019-09-13 01:15:47 +0530

    crontab working with sending error logs in mail

--- script.py
+++ script.py
@@ -1,65 +1,53 @@
 import os
+import subprocess
 
-# BASE_DIR = os.environ["BASE_DIR"]
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+from datetime import date
+from glob import glob
+from io import StringIO
+
+from secrets import fromaddr,toaddr,password
+
+
+BASE_DIR = os.environ["BASE_DIR"]
 
 
 # for debugging
-BASE_DIR = "/home/jigar/projects/hackerRankLeaderBoard"
+# BASE_DIR = "/home/jigar/projects/hackerRankLeaderBoard"
 
-
-# server_addr = "http://localhost:8080/leaderboard"
-# os.system("java -jar target/hackerRankLeaderboard-0.0.1-SNAPSHOT.jar 1")
 
 # for debugging
-# server_addr = "http://127.0.0.1:8887/Pipfile"
+# result = subprocess.run(['java', '-cp',BASE_DIR,'hello'],stderr=subprocess.PIPE)
 
-# -O => output file to $filename
-filename = "leaderboard.xlsx"
+exjar = os.path.join(BASE_DIR,'target',"hackerRankLeaderboard-0.0.1-SNAPSHOT.jar")
 
-# for debugging
-# filename = "zzz.txt"
+result = subprocess.run(['java','-jar' ,exjar,"1"],stderr=subprocess.PIPE)
 
-# os.system(f"wget -O {os.path.join(BASE_DIR,filename)} {server_addr}")
+errors = []
 
-##send email code from gfg
+if(result.stderr):
+	errors.append("Errors in new grads profile tracking..\n\n\n");
+	errors.append(result.stderr.decode("utf-8"))
 
-def send_email():
+result = subprocess.run(['java','-jar' ,exjar,"2"],stderr=subprocess.PIPE)
 
-	# Python code to illustrate Sending mail with attachments
-	# from your Gmail account
+if(result.stderr):
+	if(errors):
+		errors.append("\n\n\n");
+		errors.append("#"*100)
+		errors.append("\n\n\n");
+	errors.append("Errors in employees profile tracking..\n\n\n");
+	errors.append(result.stderr.decode("utf-8"))
 
-	# libraries to be imported
-	import smtplib
-	from email.mime.multipart import MIMEMultipart
-	from email.mime.text import MIMEText
-	from email.mime.base import MIMEBase
-	from email import encoders
-	from datetime import date
-	from secrets import fromaddr,toaddr,password
+# print("".join(errors))
 
-	# instance of MIMEMultipart
-	msg = MIMEMultipart()
 
-	# storing the senders email address
-	msg['From'] = fromaddr
-
-	# storing the receivers email address
-	msg['To'] = ",".join(toaddr) if type(toaddr) is list else toaddr
-
-	# storing the subject
-	msg['Subject'] = "leaderboard as of date " +date.today().strftime("%d/%m/%y")
-
-	# string to store the body of the mail
-	body = "PFA"
-	# body = "not the leaderboard just testing"
-
-	# attach the body with the msg instance
-	msg.attach(MIMEText(body, 'plain'))
-
-	# open the file to be sent
-
-	# filename = filename
-
+def prepare_attachment(filename):
 	attachment = open(os.path.join(BASE_DIR,filename), "rb")
 
 	# instance of MIMEBase and named as p
@@ -73,8 +61,59 @@ def send_email():
 
 	p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
 
-	# attach the instance 'p' to instance 'msg'
-	msg.attach(p)
+	return p
+
+
+##send email code from gfg
+def send_email():
+
+	# instance of MIMEMultipart
+	msg = MIMEMultipart()
+
+	# storing the senders email address
+	msg['From'] = fromaddr
+
+	# storing the receivers email address
+	msg['To'] = ",".join(toaddr) if type(toaddr) is list else toaddr
+
+	# storing the subject
+	if(errors == []):
+		msg['Subject'] = "leaderboard as of date " +date.today().strftime("%d/%m/%y")
+
+	# string to store the body of the mail
+	body = "PFA"
+	# body = "not the leaderboard just testing"
+
+	# attach the body with the msg instance
+	msg.attach(MIMEText(body, 'plain'))
+
+
+	for filename in glob(os.path.join(BASE_DIR,"leaderboard_*.xlsx")):
+
+		p = prepare_attachment(os.path.basename(filename))
+
+		# attach the instance 'p' to instance 'msg'
+		msg.attach(p)
+
+	if(errors):
+
+		logfile = "error_logs.txt"
+
+		msg['Subject'] = f"ERROR : Attaching previous weeks leaderboard, check {logfile}"
+
+		with StringIO("".join(errors)) as f:
+			# instance of MIMEBase and named as p
+			p = MIMEBase('application', 'octet-stream')
+
+			# To change the payload into encoded form
+			p.set_payload((f).read())
+
+			# encode into base64
+			encoders.encode_base64(p)
+
+			p.add_header('Content-Disposition', f"attachment; filename= {logfile}")
+
+			msg.attach(p)
 
 	# creates SMTP session
 	s = smtplib.SMTP("smtp.gmail.com", 587)
--- setup_cron.py
+++ setup_cron.py
@@ -1,6 +1,9 @@
 from crontab import CronTab
 import os
 
+#for debugging
+# $ grep CRON /var/log/syslog
+
 def setup_cron():
     user_cron  = CronTab(user=True)
     script_path = os.path.dirname(os.path.abspath(__file__))
@@ -20,6 +23,8 @@ def setup_cron():
     ## every morning 8 am uncomment when needed
     job.hour.on(8)
     ## for testing comment when done
+    # job.hour.on(1)
+    # job.minute.on(9)
     # job.minute.every(1)
 
     user_cron.write()
